c2b17db773c56e53f741679738923817276a414b
Keshav Gautam <keshavg1209@gmail.com>
2023-09-08 23:16:23 +0530

    feat: add mongoDB models

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -8,6 +8,8 @@ import io
 import json
 from PIL import Image
 from concurrent.futures import ThreadPoolExecutor
+import db
+from bson.json_util import dumps, loads
 
 app = Flask(__name__)
 CORS(app)
@@ -21,12 +23,13 @@ def optimisePrompt():
     url ="https://api.edenai.run/v2/text/prompt_optimization"
     payload = {
         "providers": "openai",
-        "text": prompt,
+        "text": "Alabasta Bakery is a chain that delivers delicious bakery items like cakes, breads, pastries, cookies and many similar packaged items. Need to create an image for email advertisement for the upcoming Indian Independence day for all the users getting 20 percent discount on all the products.",
         "target_provider" : "google"
     }
     response = requests.post(url, json=payload, headers=headers)
 
     result = json.loads(response.text)
+    # print(result['openai'])
     return result['openai']
 
 def generateImg(prompt):
@@ -132,6 +135,73 @@ def generate():
 #     response = requests.post(API_URL, headers=headers, json=payload)
 #     print(response)
 #     return response.content
+
+@app.route("/test")
+def add_campaign2():
+    db.db.campaigns.insert_one({"name": "John"})
+    return "Connected to the data base!"
+
+@app.route('/add_campaign', methods=["POST"])
+def add_campaign():
+    post_data = request.get_json()
+    try:
+        campaign_doc = {
+                        'id': post_data['id'],
+                        'title': post_data['title'],
+                        'raw_prompt': post_data['raw_prompt'],
+                        'engineered_prompt': post_data['engineered_prompt'],
+                        'generated_content': post_data['generated_content'],
+                        'base64_img': post_data['base64_img'],
+                        'created_date': post_data['created_date'],
+                        'schedule_date': post_data['schedule_date'],
+                        'medium': post_data['medium']
+                    }
+        db.db.campaigns.insert_one(campaign_doc)
+        return jsonify({"response": "success"}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/update_campaign', methods=['POST'])
+def query_records():
+    post_data = request.get_json()
+    id = post_data['id']
+    title = post_data['title']
+    schedule_date = post_data['schedule_date']
+    generated_content = post_data['generated_content']
+    if not id:
+        return jsonify({'error': 'campaign not found'})
+    else:
+        response = db.db.campaigns.update_one(
+            { "id": id },
+            { "$set": { "title ": title, "schedule_date" : schedule_date, "generated_content":generated_content } }
+        )
+        return jsonify({"response": "success"}), 200
+    
+@app.route('/delete_campaign', methods=["DELETE"])
+def delete_campaign():
+    post_data = request.get_json()
+    try:
+        id = post_data['id']
+        response = db.comments.delete_one( { "id": id } )
+
+        return jsonify({"response": "success"}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/search', methods=["GET"])
+def search_campaign():
+    try:
+        campaign_records = db.db.campaigns.find({})
+        records = []
+        list_cur = list(campaign_records)
+  
+        # Converting to the JSON
+        json_data = dumps(list_cur, indent = 2)
+        print(json_data)
+        return json_data
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 def main():
     print(compare_with_base())
