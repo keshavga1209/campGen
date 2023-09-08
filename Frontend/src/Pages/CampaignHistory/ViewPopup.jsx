a45e3abf4951d9b5f7efd8c266d1ee75388b9eb5
neel-thakker <neelthakker2001@gmail.com>
2023-09-09 02:44:57 +0530

    add intgrated Table

---
+++ Frontend/src/Pages/CampaignHistory/ViewPopup.jsx
@@ -0,0 +1,108 @@
+import { IoCloseCircle } from "react-icons/io5";
+import { FcPrevious } from "react-icons/fc";
+import { useEffect } from "react";
+
+export default function ViewPopup({ setIsViewing, campaign }) {
+	// useEffect(() => {
+	// 	console.log(
+	// 		campaign.created_date.substring(0, campaign.schedule_date.length)
+	// 	);
+	// });
+
+	return (
+		<div className="fixed top-0 left-0 flex justify-center items-center h-full w-full backdrop-blur-sm z-10">
+			<div className="relative bg-white rounded-lg h-[24rem] w-[28rem] shadow-lg flex flex-col items-center">
+				<button
+					className="cursor-pointer absolute top-2 h-7 left-2 bg-gray-200 hover:bg-gray-400 flex gap-2 items-center rounded-xl py-1 px-3"
+					onClick={(_) => setIsViewing(-1)}>
+					<FcPrevious />
+					Back
+				</button>
+
+				<span
+					className="cursor-pointer absolute top-2 right-2"
+					onClick={(_) => setIsViewing(-1)}>
+					{" "}
+					<IoCloseCircle size={"25px"} />{" "}
+				</span>
+
+				<h1 className="p-2 text-lg text-gray-400 w-full text-center border-b border-gray-300">
+					More info about the Campaign
+				</h1>
+
+				<form className="mt-4 w-full px-8">
+					<div>
+						<label
+							className="block text-grey-darker text-sm font-bold mb-2"
+							htmlFor="username">
+							Title
+						</label>
+						<input
+							disabled
+							required
+							type="text"
+							name="title"
+							className="appearance-none h-full rounded-lg bg-gray-300 block w-full border-gray-400 text-gray-700 py-2 px-2 leading-tight focus:outline-none focus:border-gray-500"
+							placeholder="Enter title..."
+							value={campaign.title}
+						/>
+					</div>
+
+					<div className="mt-4">
+						<label
+							className="block text-grey-darker text-sm font-bold mb-2"
+							htmlFor="username">
+							Medium
+						</label>
+						<input
+							disabled
+							required
+							name="medium"
+							className="appearance-none h-full rounded-lg bg-gray-300 block w-full border-gray-400 text-gray-700 py-2 px-2  leading-tight focus:outline-none focus:border-gray-500"
+							value={campaign.medium}
+						/>
+					</div>
+
+					<div className="mt-4">
+						<label
+							className="block text-grey-darker text-sm font-bold mb-2"
+							htmlFor="username">
+							Created Date-Time
+						</label>
+						<input
+							disabled
+							required
+							type="datetime-local"
+							name="scheduledDate"
+							value={campaign.created_date.substring(
+								0,
+								campaign.schedule_date.length
+							)}
+							className="appearance-none h-full rounded-lg bg-gray-300 block w-full border-gray-400 text-gray-700 py-2 px-2 leading-tight focus:outline-none focus:border-gray-500"
+						/>
+					</div>
+
+					<div className="mt-4">
+						<label
+							className="block text-grey-darker text-sm font-bold mb-2"
+							htmlFor="username">
+							Launch Date-Time
+						</label>
+						<input
+							disabled
+							required
+							type="datetime-local"
+							name="scheduledDate"
+							value={campaign.schedule_date}
+							className="appearance-none h-full rounded-lg bg-gray-300 block w-full border-gray-400 text-gray-700 py-2 px-2 leading-tight focus:outline-none focus:border-gray-500"
+						/>
+					</div>
+
+					{/* <button className="mt-6 w-full mx-auto  rounded-md bg-green-600 hover:bg-green-400 py-2 px-5 text-white">
+						Submit
+					</button> */}
+				</form>
+			</div>
+		</div>
+	);
+}
--- Frontend/src/Pages/CampaignHistory/index.jsx
+++ Frontend/src/Pages/CampaignHistory/index.jsx
@@ -1,13 +1,156 @@
 import MainPanelLayout from "../../Components/Layout/MainPanelLayout";
-import {IoAddCircleSharp} from 'react-icons/io5'
+import { IoAddCircleSharp } from "react-icons/io5";
 import Table from "../../Components/Table";
 import { DUMMY_TABLE_DATA } from "../../Components/Table/dummyData";
+import { request } from "../../Utils/request";
+import { useEffect, useState } from "react";
 
+import { IconContext } from "react-icons";
+import { FaRegEdit } from "react-icons/fa";
+import { BsEyeFill } from "react-icons/bs";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import Loader from "../../Components/Loader";
+import ViewPopup from "./ViewPopup";
 
-export default function CampaignHistory(props){
-  return (
-    <MainPanelLayout title={"Campaign History"}>
-      <Table data={DUMMY_TABLE_DATA} />
-    </MainPanelLayout>
-  )
+export default function CampaignHistory(props) {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [isViewing, setIsViewing] = useState(-1);
+	const [isEditing, setIsEditing] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
+
+	const [campaigns, setCampaigns] = useState([]);
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetchCampaign();
+	}, []);
+
+	const handleEditClick = (i) => {
+		console.log("Edit button clicked", i);
+	};
+
+	const handleViewClick = (i) => {
+		console.log("View button clicked", i);
+		setIsViewing(i);
+	};
+
+	const handleDeleteClick = (i) => {
+		console.log("Delete button clicked", i);
+	};
+
+	const fetchCampaign = async () => {
+		const [err, res] = await request("get", "/search");
+
+		console.log(err, res);
+
+		if (err === "network_error") {
+			window.alert("check your network and try again");
+			return;
+		}
+
+		if (err !== null) {
+			window.alert(
+				JSON.stringify(err) + "\n Contact Us! give this error"
+			);
+			return;
+		}
+
+		setCampaigns(res);
+		setIsLoading(false);
+	};
+
+	const generateData = () => {
+		let data = {
+			headings: [
+				"Sr. No.",
+				"Title",
+				"Lauch Date",
+				"Created Date",
+				"Medium",
+				"Actions",
+			],
+			rows: campaigns.map((c, i) => {
+				return [
+					<span className="text-gray-900 whitespace-no-wrap">
+						{" "}
+						{i + 1}{" "}
+					</span>,
+					<span className="text-gray-900 whitespace-no-wrap">
+						{" "}
+						{c.title}{" "}
+					</span>,
+					<span className="text-gray-900 whitespace-no-wrap">
+						{" "}
+						{c.schedule_date}{" "}
+					</span>,
+					<span className="text-gray-900 whitespace-no-wrap">
+						{" "}
+						{c.created_date}{" "}
+					</span>,
+					<span className="text-gray-900 whitespace-no-wrap">
+						{" "}
+						{c.medium}{" "}
+					</span>,
+					<span className="flex gap-4">
+						<IconContext.Provider
+							value={{ color: "blue", size: "20px" }}>
+							{" "}
+							<BsEyeFill
+								onClick={() => handleViewClick(i)}
+								className="hover:cursor-pointer"
+							/>{" "}
+						</IconContext.Provider>
+
+						<IconContext.Provider
+							value={{ color: "blue", size: "20px" }}>
+							{" "}
+							<FaRegEdit
+								onClick={() => handleEditClick(i)}
+								className="hover:cursor-pointer"
+							/>{" "}
+						</IconContext.Provider>
+
+						<IconContext.Provider
+							value={{ color: "red", size: "20px" }}>
+							{" "}
+							<RiDeleteBin5Line
+								onClick={() => handleDeleteClick(i)}
+								className="hover:cursor-pointer"
+							/>{" "}
+						</IconContext.Provider>
+					</span>,
+				];
+			}),
+		};
+
+		// console.log(data);
+		// console.log(DUMMY_TABLE_DATA);
+
+		return data;
+	};
+
+	return (
+		<MainPanelLayout title={"Campaign History"}>
+			{isLoading && (
+				<div className="absolute top-0 left-0 flex justify-center items-center h-full w-full backdrop-blur-sm z-10">
+					<div className="relative bg-white rounded-lg h-[22rem] w-[24rem] shadow-lg flex flex-col items-center justify-center">
+						<Loader />
+					</div>
+				</div>
+			)}
+
+			{isViewing >= 0 && (
+				<ViewPopup
+					setIsViewing={setIsViewing}
+					campaign={campaigns[isViewing]}
+				/>
+			)}
+
+			<>
+				{/* <Table data={DUMMY_TABLE_DATA} /> */}
+				<Table data={generateData()} />
+			</>
+		</MainPanelLayout>
+	);
 }
