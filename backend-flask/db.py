d053b304a731200436a604f75f1181a2943c1a25
GrayFlash <gk74533@gmail.com>
2023-09-15 12:17:51 +0530

    fix: remove conn string

--- backend-flask/db.py
+++ backend-flask/db.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_pymongo import pymongo
+from dotenv import load_dotenv
 
-CONNECTION_STRING = "mongodb+srv://keshg0912:test123@cluster0.fldo7z7.mongodb.net/?retryWrites=true&w=majority"
+CONNECTION_STRING = environ.get('MONGO_CONNECTION_STRING')
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('rakathon_db')
 collection = pymongo.collection.Collection(db, 'campaigns')
